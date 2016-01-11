cba0b4a9647136798dec944448d252cdca54f0de
Rob Erskine <Rob@RobErskine.com>
2016-01-11 09:26:52 -0500

    adding animation and transitions

--- src/components/Main.js
+++ src/components/Main.js
@@ -16,8 +16,10 @@ class AppComponent extends React.Component {
  	}
  	rotateImages(){
  		var that = this;
+ 		var timer = 6000;
 
  		setInterval(function(){
+
 		 	if(that.state.count === (that.state.dribbbleList.length - 1)){
 	 			that.setState({
 	 				count: 0
@@ -27,7 +29,7 @@ class AppComponent extends React.Component {
  			that.setState({
 				count: that.state.count + 1
 			});
- 		}, 6000);
+ 		}, timer);
  	}
  	loadImages(){
 	  	var that = this;
@@ -83,7 +85,7 @@ class AppComponent extends React.Component {
 		if (!this.state.dribbbleList) {
 			return(
 				<div className="loading-screen">
-					<p>now loading</p>
+					<div className="hh-logo" dangerouslySetInnerHTML={{__html: '<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60"><g fill="#231F20"><path d="M29.458 53.664V6.336l-9.504 4.945v13.65L8.11 26.62l-.01-9.15-7.817 4.053v19.86L8.1 44.674V34.268l11.854.554v14.846l9.504 3.996zM51.9 17.47l-.01 9.148-11.843-1.687V11.28l-9.505-4.944v47.328l9.505-3.996V34.822l11.854-.554v10.407l7.817-3.296V21.52L51.9 17.47z"/></g></svg>'}}></div>
 				</div>
 			)
 		}
@@ -100,6 +102,12 @@ class AppComponent extends React.Component {
 				img = current.images.normal;
 			}
 
+			var description;
+
+			if(current.description){
+				description = <div dangerouslySetInnerHTML={{__html : current.description}}></div>;
+			}
+
 			return (
 	    		<div className="index">
 	    			<section>
@@ -107,18 +115,21 @@ class AppComponent extends React.Component {
 	    					<img src={img} alt={current.username}/>
 	    				</div>
 	    				<div className="content-holder">
-	    					<header>
-		    					<h2>{current.username}</h2>
-		    					<ul>
-		    						<li><span dangerouslySetInnerHTML={{__html: '<svg class="icon icon-eye"><use xlink:href="#icon-eye"></use></svg>'}}></span> {current.views_count}</li>
-		    						<li><span dangerouslySetInnerHTML={{__html: '<svg class="icon icon-comment"><use xlink:href="#icon-comment"></use></svg>'}}></span> {current.comments_count}</li>
-		    						<li><span dangerouslySetInnerHTML={{__html: '<svg class="icon icon-heart"><use xlink:href="#icon-heart"></use></svg>'}}></span> {current.likes_count}</li>
-		    					</ul>
-	    					</header>
-			 				<div dangerouslySetInnerHTML={{__html : current.description}}></div>
+	    					<div className="content">
+		    					<header>
+			    					<h2>{current.username}</h2>
+			    					<ul>
+			    						<li><span dangerouslySetInnerHTML={{__html: '<svg class="icon icon-eye"><use xlink:href="#icon-eye"></use></svg>'}}></span> {current.views_count}</li>
+			    						<li><span dangerouslySetInnerHTML={{__html: '<svg class="icon icon-comment"><use xlink:href="#icon-comment"></use></svg>'}}></span> {current.comments_count}</li>
+			    						<li><span dangerouslySetInnerHTML={{__html: '<svg class="icon icon-heart"><use xlink:href="#icon-heart"></use></svg>'}}></span> {current.likes_count}</li>
+			    					</ul>
+		    					</header>
+				 				{description}
+			 				</div>
 	    				</div>
 		 			</section>
 		 			<div className="background-image" style={{backgroundImage: 'url(' + img + ')'}}></div>
+		 			<div className="hh-logo" dangerouslySetInnerHTML={{__html: '<svg xmlns="http://www.w3.org/2000/svg" width="60" height="60" viewBox="0 0 60 60"><g fill="#231F20"><path d="M29.458 53.664V6.336l-9.504 4.945v13.65L8.11 26.62l-.01-9.15-7.817 4.053v19.86L8.1 44.674V34.268l11.854.554v14.846l9.504 3.996zM51.9 17.47l-.01 9.148-11.843-1.687V11.28l-9.505-4.944v47.328l9.505-3.996V34.822l11.854-.554v10.407l7.817-3.296V21.52L51.9 17.47z"/></g></svg>'}}></div>
 	    		</div>
 	    	);
 		}
