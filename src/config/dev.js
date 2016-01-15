45c387c3f1ea736cabbe19414304e163056c939c
Rob Erskine <Rob@RobErskine.com>
2016-01-15 13:08:50 -0500

    adjusting screensaver to download as raw

--- src/config/dev.js
+++ src/config/dev.js
@@ -6,7 +6,7 @@ import baseConfig from './base';
 let config = {
   appEnv: 'dev',
   apiUrl: 'http://dribbble-screensaver-api.hhcc.dev/',
-  screensaverUrl: 'https://github.com/hillholliday/creative-screensaver/blob/master/src/screensaver/HHCC Creative.saver.zip'
+  screensaverUrl: 'https://github.com/hillholliday/creative-screensaver/blob/master/src/screensaver/HHCC%20Creative.saver.zip?raw=true'
 };
 
 export default Object.freeze(Object.assign({}, baseConfig, config));
